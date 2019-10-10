a61038e85666f7ea1d907a416d941522d415d2e8
f-nakahara <meshiprio503@gmail.com>
2019-10-10 14:05:13 +0900

    none

--- scripts/admin.js
+++ scripts/admin.js
@@ -11,10 +11,10 @@ function append_room() {
     })
         .done(function (data) {
             console.log("ルームリストを更新しました。");
-            $(".join_room").text("");
+            $(".room_list").text("");
             if (!Object.keys(data).length) {
                 console.log("現在ルームはありません。");
-                $(".join_room").append("<div class='content'><h3 class='room_name'>" + "現在作成されてるルームはありません" + "</h3></div>")
+                $(".room_list").append("<div class='content'><h3 class='room_name'>" + "現在作成されてるルームはありません" + "</h3></div>")
             }
             else {
                 console.log(data);
@@ -24,7 +24,7 @@ function append_room() {
                     console.log(room_name + "は満員です。");
                 }
                 else {
-                    $(".join_room").append("<div class='content'><h3 class='room_name'>" + room_name + "</h3>" + "<button class='delete_room_btn'>削除</button></div>");
+                    $(".room_list").append("<div class='content'><h3 class='room_name'>" + room_name + "</h3>" + "<button class='delete_room_btn'>削除</button></div>");
                 }
 
             }
@@ -47,16 +47,16 @@ function append_player() {
     })
         .done(function (data) {
             console.log("プレイヤーリストを更新しました。");
-            $(".create_room").text("");
+            $(".player_list").text("");
             if (!Object.keys(data).length) {
                 console.log("現在プレイヤーはいません");
-                $(".create_room").append("<div class='content'><h3 class='player_name'>" + "現在プレイヤーはいません" + "</h3></div>")
+                $(".player_list").append("<div class='content'><h3 class='player_name'>" + "現在プレイヤーはいません" + "</h3></div>")
             }
             else {
                 console.log(data);
             }
             for (player_name in data) {
-                $(".create_room").append("<div class='content'><h3 class='player_name'>" + player_name + "</h3>" + "<button class='delete_player_btn'>削除</button></div>");
+                $(".player_list").append("<div class='content'><h3 class='player_name'>" + player_name + "</h3>" + "<button class='delete_player_btn'>削除</button></div>");
             }
         })
         .fail(function (data) {
@@ -112,51 +112,51 @@ function delete_player() {
     });
 }
 
-function show_create_room() {
-    $("#create_room_link").on("click", function (e) {
+function show_player_list() {
+    $("#player_link").on("click", function (e) {
         e.preventDefault();
-        $(".create_room").show();
-        hide_join_room();
+        $(".player_list").show();
+        hide_room_list();
     });
 }
 
-function show_join_room() {
-    $("#join_room_link").on("click", function (e) {
+function show_room_list() {
+    $("#room_link").on("click", function (e) {
         e.preventDefault();
         append_room();
-        $(".join_room").show();
+        $(".room_list").show();
         // $("content").show();
-        hide_create_room();
+        hide_player_list();
     });
 }
 
-function show_create_room() {
-    $("#create_room_link").on("click", function (e) {
+function show_player_list() {
+    $("#player_link").on("click", function (e) {
         e.preventDefault();
         append_player();
-        $(".create_room").show();
+        $(".player_list").show();
         // $("content").show();
-        hide_join_room();
+        hide_room_list();
     });
 }
 
-function hide_create_room() {
-    $(".create_room").hide();
+function hide_player_list() {
+    $(".player_list").hide();
 }
 
-function hide_join_room() {
-    $(".join_room").hide();
+function hide_room_list() {
+    $(".room_list").hide();
 }
 
 window.onload = function () {
     this.append_player();
-    this.hide_join_room();
-    this.hide_create_room();
+    this.hide_room_list();
+    this.hide_player_list();
 }
 
 $(function () {
-    show_create_room();
-    show_join_room();
+    show_player_list();
+    show_room_list();
     delete_room();
     delete_player();
 })
