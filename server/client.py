49d4df9906316c16c6b6da264990460f5099a531
da4lin <ryo.wasenet0821@gmail.com>
2019-10-10 19:50:35 +0900

    Update client.py
    魔改造。現状固定メッセージを送るだけ。ローカルサーバーで動作確認済。実際のページでは未確認。

--- server/client.py
+++ server/client.py
@@ -1,16 +1,47 @@
 import socket
-import time
+import random
 import os,sys
+import tkinter as tk
+import threading
+import msvcrt
 
-#とりあえずの確認(あらかじめ建てた鯖とつなげるか？))
-if __name__ == "__main__":
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+class Client():
+    def __init__(self,host,port):
+        self.host = host
+        self.port = port
+        self.counter = 0
+        self.me = random.randint(1,100)
+
+    def cnct_server(self):
+        print('YOUR ID is : {}'.format(self.me))
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # サーバを指定
-        s.connect(('localhost', 3000))
-        # サーバにメッセージを送る
-        s.sendall(b'hello')
-        # ネットワークのバッファサイズは1024。サーバからの文字列を取得する
-        data = s.recv(1024)
-        #
-        print(repr(data))
-        time.sleep(3)
+        self.sock.connect((self.host, self.port))
+        thread = threading.Thread(target=self.send_massage,args=())
+        thread.start()
+        while True:
+            try:
+                data = self.sock.recv(4096)
+                if data:
+                    print(data.decode())
+            except KeyboardInterrupt:
+                break
+
+    def send_massage(self):
+        while True:
+            key = ord(msvcrt.getch())
+            if key == 13:
+                self.counter += 1
+                massage = 'This is CLIENT ID:' + "{0:02d}".format(self.me) + 'count: {}'.format(self.counter)
+                # サーバにメッセージを送る
+                #print(massage)
+                self.sock.sendall(massage.encode())
+            elif key == 27:
+                break
+
+#ローカル動作確認用
+if __name__ == "__main__":
+    host = 'localhost'
+    port = 3000
+    client = Client(host,port)
+    client.cnct_server()
